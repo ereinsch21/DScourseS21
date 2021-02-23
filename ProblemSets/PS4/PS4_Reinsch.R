8b91de3810cbfe678ac7f717e9855a362a9cf8dd
ereinsch21 <78052238+ereinsch21@users.noreply.github.com>
2021-02-23 11:30:29 -0600

    Add files via upload

---
+++ ProblemSets/PS4/PS4_Reinsch.R
@@ -0,0 +1,11 @@
+library(jsonlite)
+
+library(tidyverse)
+system(wget -O dates.json "https://www.vizgr.org/historical-events/search.php?format=json&begin_date=00000101&end_date=20210219&lang=en")
+cat dates.json
+mylist<-fromJSON('dates.json')
+mydf<-bind_rows(mylist$result[-1])
+class(mydf$date)
+mydf%>% head %>% print
+print(class(mydf$date))
+
